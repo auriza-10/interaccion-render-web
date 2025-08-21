440b0114644c51efa6f178af06fefeb131008305
auriza-10 <auriza@centro.edu.mx>
2025-08-21 14:33:26 -0600

    cambie cosas de mi ejercicio 3

--- sesion-03/ejercicio-03/app.js
+++ sesion-03/ejercicio-03/app.js
@@ -2,15 +2,7 @@ console.log("S03. Ejercicio 03");
 
 console.log(gsap);
 
-gsap.to(
-    ".rectangulo",
-    {
-        x: 500,
-        duration: 5, //segundos
-        ease: "bounce.inOut",
 
-    }
-);
 
 window.addEventListener("mousedown", function() {
     gsap.to(
@@ -18,7 +10,18 @@ window.addEventListener("mousedown", function() {
     {
         x: 500,
         duration: 5, //segundos
-        ease: "bounce.inOut",
+        ease: "power1.inOut",
+        onComplete: function(){
+            gsap.to(
+                ".rectangulo",
+                {
+                    y: 0,
+                    x: 1000,
+                    duration: 5,
+                    ease: "power1.inOut",
+                }
+            )
+        }
 
     }
 );
