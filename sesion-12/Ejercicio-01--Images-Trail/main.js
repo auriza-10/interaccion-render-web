eee1b6ed139c6eb5ecdf0aa3e8967050d264a927
auriza-10 <auriza@centro.edu.mx>
2025-11-06 15:10:49 -0600

    Cambié el orden de presentación de imagenes, tanto su aparición, movimiento, etc.

--- sesion-12/Ejercicio-01--Images-Trail/main.js
+++ sesion-12/Ejercicio-01--Images-Trail/main.js
@@ -1,5 +1,20 @@
 console.log('main.js');
 
+/*
+1.Definir en una constante el espacio mínimo que deseamos entre cada imagen.
+2. Crear una variable tipo objeto para guardar la posición de la última imagen creada y poder medir la distancia al mouse.
+3. En el evento “mousemove” donde estamos invocando la función para crear imágenes, vamos a definir la condición para crear la imagen:
+4. Primero calculamos la distancia en X y Y entre la posición de la última imagen y el mouse actualmente.
+5. Si la distancia calculada es mayor a nuestro espacio mínimo, solo entonces creamos la imagen.
+6. Finalmente, actualizamos el registro de la posición de la última imagen ya que creamos una nueva. */
+
+const minDistance = 200;
+var lastPost= {
+x: 0,
+y: 0
+}
+
+
 // 04. Mostrarlas en ciclo.
 /* 
 1. Crear una lista con la ubicación de las imágenes a renderizar.
@@ -44,10 +59,12 @@ function createFloatImage(posX, posY) {
     img.style.left = posX - (113.5) + "px";
     img.style.position = "absolute";
     img.style.opacity= 0;
+    img.style.zIndex = Math.round(Math.random() * 10);
     document.body.appendChild(img);
 
     gsap.to(img, {
         opacity: 1,
+        y: "-20px",
         duration: 1,
         ease: "power3.out"
     })
@@ -73,7 +90,21 @@ function createFloatImage(posX, posY) {
 
 // 02. Renderizar “n” imágenes.
 window.addEventListener('mousemove', function (eventData) {
+    // clacular distancia entre el mouse y la última imagen creada para evaluar nuestra condición para renderizar o no.
+    //1. Clacular distancia
+    var dx = eventData.clientX - lastPost.x;
+    dx = Math.abs(dx); // valor absoluto
+
+    var dy = eventData.clientY - lastPost.y;
+    dy = Math.abs(dy); // valor absoluto
+
+    //2. Evaluar la condición
+
+    if (dx >= minDistance || dy >= minDistance) {
     createFloatImage(eventData.clientX, eventData.clientY);
+    lastPost.x = eventData.clientX;
+    lastPost.y = eventData.clientY;
+    }
 });
 
 
